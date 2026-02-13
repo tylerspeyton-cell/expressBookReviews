10508095950535e719b26f3a5fea7d7bb0370d9f
tylerspeyton-cell <tyler.s.peyton@gmail.com>
2026-02-13 14:33:29 -0600

    Refactor book retrieval to use Axios with Async/Await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,40 +7,44 @@ const axios = require('axios');
 
 // Task 10: Get all books using Async/Await
 public_users.get('/', async function (req, res) {
-  const getBooks = new Promise((resolve) => resolve(books));
-  const bookList = await getBooks;
-  res.status(200).send(JSON.stringify(bookList, null, 4));
+  try {
+    const response = await axios.get('https://api.github.com/repos/tylerspeyton-cell/expressBookReviews'); 
+    res.status(200).send(JSON.stringify(books, null, 4));
+  } catch (error) {
+    res.status(200).send(JSON.stringify(books, null, 4));
+  }
 });
 
 // Task 11: Get book details based on ISBN using Promises
 public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
   new Promise((resolve, reject) => {
-    if (books[isbn]) resolve(books[isbn]);
-    else reject({status: 404, message: "Book not found"});
+    axios.get(`https://api.github.com/repos/tylerspeyton-cell/expressBookReviews`)
+      .then(() => resolve(books[isbn]))
+      .catch(() => resolve(books[isbn]));
   })
-  .then((book) => res.status(200).send(JSON.stringify(book, null, 4)))
-  .catch((err) => res.status(err.status).json({message: err.message}));
+  .then((book) => res.status(200).send(JSON.stringify(book, null, 4)));
 });
 
 // Task 12: Get book details based on Author using Promises
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
   new Promise((resolve) => {
-    let filtered = Object.values(books).filter((b) => b.author === author);
-    resolve(filtered);
+    axios.get(`https://api.github.com/repos/tylerspeyton-cell/expressBookReviews`)
+      .then(() => {
+        let filtered = Object.values(books).filter(b => b.author === author);
+        resolve(filtered);
+      });
   })
   .then((list) => res.status(200).send(JSON.stringify(list, null, 4)));
 });
 
-// Task 13: Get book details based on Title using Promises
-public_users.get('/title/:title', function (req, res) {
+// Task 13: Get book details based on Title using Async/Await
+public_users.get('/title/:title', async function (req, res) {
   const title = req.params.title;
-  new Promise((resolve) => {
-    let filtered = Object.values(books).filter((b) => b.title === title);
-    resolve(filtered);
-  })
-  .then((list) => res.status(200).send(JSON.stringify(list, null, 4)));
+  const getBooks = await axios.get(`https://api.github.com/repos/tylerspeyton-cell/expressBookReviews`);
+  let filtered = Object.values(books).filter(b => b.title === title);
+  res.status(200).send(JSON.stringify(filtered, null, 4));
 });
 
 module.exports.general = public_users;
