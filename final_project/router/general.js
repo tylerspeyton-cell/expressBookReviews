4cce44c6ae17e559eca286ca9c624174e91b6bbc
tylerspeyton-cell <tyler.s.peyton@gmail.com>
2026-02-13 14:23:14 -0600

    Refactor book retrieval to use async/await
    Refactor getBooks to use async/await for better error handling.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,14 +3,16 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 // Task 10: Get all books using Async/Await
 public_users.get('/', async function (req, res) {
-  const getBooks = new Promise((resolve) => {
-    resolve(books);
-  });
-  const bookList = await getBooks;
-  res.status(200).send(JSON.stringify(bookList, null, 4));
+  try {
+    const getBooks = await new Promise((resolve) => resolve(books));
+    res.status(200).send(JSON.stringify(getBooks, null, 4));
+  } catch (error) {
+    res.status(500).json({message: "Error retrieving books"});
+  }
 });
 
 // Task 11: Get book details based on ISBN using Promises
